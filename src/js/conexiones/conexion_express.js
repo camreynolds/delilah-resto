f3b0de6c08852a2943589b071bd51aeca4fb60d5
Carlos Muñoz <camr.personal@gmail.com>
2021-03-19 17:53:48 -0500

    Se instala el paquete jsonwebtoken

--- src/js/conexiones/conexion_express.js
+++ src/js/conexiones/conexion_express.js
@@ -1,5 +1,6 @@
 require ("dotenv").config;
 const express = require("express");
+const jwt = require('jsonwebtoken');
 const app = express();
 
 app.use(express.json({limit: '10Kb'}));
--- src/js/endpoints/usuarios_endpoints.js
+++ src/js/endpoints/usuarios_endpoints.js
@@ -1,9 +1,12 @@
 const app = require('../conexiones/conexion_express.js');
 const sequelize = require('../conexiones/conexion_sequelize.js');
 
+//Enpoint que devuelve todos los registros de la tabla "usuarios".
 app.get('/usuarios', async (req, res) => {
+    let nombreTabla = 'usuarios';
+    let tipoDeQuery = `SELECT * FROM ${nombreTabla}`;
     
-    await sequelize.query('SELECT * FROM usuarios', {type: sequelize.QueryTypes.SELECT})
+    await sequelize.query(tipoDeQuery, {type: sequelize.QueryTypes.SELECT})
         .then(data => {
             console.log(data);
             res.status(200).json({
@@ -19,12 +22,16 @@ app.get('/usuarios', async (req, res) => {
         });
 });
 
-app.post('/usuarios', async (req, res) => {
+
+// Endpoint para registar nuevos usuarios.
+app.post('/resgistrarse', async (req, res) => {
     let usuario = Object.values(req.body);
+    let nombreTabla = 'usuarios';
+    let tipoDeQuery = `INSERT INTO ${nombreTabla} (nickname, nombre_apellido, email, telefono, direccion_envio, contrasenna) VALUES (?,?,?,?,?,?)`;
+
     console.log("Body: " + usuario);
 
-    await sequelize.query('INSERT INTO usuarios (nickname, nombre_apellido, email, telefono, direccion_envio, contrasenna) VALUES (?,?,?,?,?,?)', 
-        {replacements: usuario})
+    await sequelize.query(tipoDeQuery, {replacements: usuario})
         .then(data => {
             console.log("Data log usuario: " + data);
             res.status(200).json({
